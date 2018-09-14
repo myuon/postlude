b4f55ee681c56a4d5f52dfbc5577ae5ce8351938
myuon <ioi.joi.koi.loi@gmail.com>
2018-09-15 03:23:06 +0900

    feat: get block comments and put them into other directory

--- src/Main.hs
+++ src/Main.hs
@@ -1,11 +1,14 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import System.Environment
+import Control.Monad
 import Data.Maybe
 import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
 import qualified Data.Text.ICU as Regex
+import qualified System.Directory as Dir
+import qualified System.FilePath as Path
+import System.Environment
 
 getBlockComments :: T.Text -> [T.Text]
 getBlockComments text = do
@@ -15,10 +18,26 @@ getBlockComments text = do
 isPragma :: T.Text -> Bool
 isPragma t = not (T.null t) && T.head t == '#'
 
+foldOnDir :: FilePath -> (FilePath -> T.Text -> IO ()) -> IO ()
+foldOnDir base process = do
+  paths <- Dir.listDirectory base
+
+  forM_ paths $ \filename -> do
+    let path = Path.joinPath [base, filename]
+
+    isFile <- Dir.doesFileExist path
+    if isFile then process path =<< TIO.readFile path
+    else foldOnDir path process
+
 main = do
   (path:_) <- getArgs
+  current <- Dir.getCurrentDirectory
 
-  file <- TIO.readFile path
-  mapM_ TIO.putStrLn $ filter (\c -> not (T.null c) && not (isPragma c)) $ getBlockComments file
+  let outDir = "docs"
 
+  foldOnDir path $ \filename content -> do
+    let path = Path.joinPath [outDir, filename]
+    Dir.createDirectoryIfMissing True (Path.dropFileName path)
+
+    TIO.writeFile path $ T.unlines $ filter (\c -> not (T.null c) && not (isPragma c)) $ getBlockComments content
 
