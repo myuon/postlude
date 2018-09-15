1b2f81c6c96db3282fbc273464bc13dc2225ae9e
myuon <ioi.joi.koi.loi@gmail.com>
2018-09-15 11:40:46 +0900

    chore: import RIO modules

--- src/Main.hs
+++ src/Main.hs
@@ -2,11 +2,10 @@
 module Main where
 
 import RIO
-import qualified Data.Text as T
-import qualified Data.Text.IO as TIO
+import qualified RIO.FilePath as Path
+import qualified RIO.Text as T
+import qualified RIO.Directory as Dir
 import qualified Data.Text.ICU as Regex
-import qualified System.Directory as Dir
-import qualified System.FilePath as Path
 import System.Environment
 
 getBlockComments :: T.Text -> [T.Text]
@@ -15,13 +14,13 @@ getBlockComments text = do
   catMaybes $ map (Regex.group 1) $ Regex.findAll re text
 
 isPragma :: T.Text -> Bool
-isPragma t = not (T.null t) && T.head t == '#'
+isPragma t = not (T.null t) && T.take 1 t == "#"
 
 createIndex :: FilePath -> [FilePath] -> IO ()
 createIndex base paths = do
   let path = Path.joinPath [base, "README.md"]
   Dir.createDirectoryIfMissing True base
-  TIO.writeFile path $ T.unlines $
+  writeFileUtf8 path $ T.unlines $
     ("# " `T.append` T.pack base) : "" : map (("- " `T.append`) . T.pack) paths
 
 foldOnDir :: FilePath -> (FilePath -> T.Text -> IO ()) -> (FilePath -> [FilePath] -> IO ()) -> IO ()
@@ -35,10 +34,10 @@ foldOnDir base onFile onDir = do
     isFile <- Dir.doesFileExist path
     if isFile then do
       when (Path.takeExtension path == ".hs") $ do
-        onFile path =<< TIO.readFile path
+        onFile path =<< readFileUtf8 path
     else foldOnDir path onFile onDir
 
-main = do
+main = runSimpleApp $ liftIO $ do
   (path:_) <- getArgs
   current <- Dir.getCurrentDirectory
 
@@ -49,7 +48,7 @@ main = do
         let path = Path.replaceExtensions (Path.joinPath [outDir, filename]) ".md"
         Dir.createDirectoryIfMissing True (Path.dropFileName path)
 
-        TIO.writeFile path $ T.unlines $ filter (\c -> not (T.null c) && not (isPragma c)) $ getBlockComments content)
+        writeFileUtf8 path $ T.unlines $ filter (\c -> not (T.null c) && not (isPragma c)) $ getBlockComments content)
     (\base paths -> do
         createIndex (Path.joinPath [outDir, base]) paths)
 
