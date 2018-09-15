6bc4338e4c8ac2fdf1fdd0926ad45a7a04b8a1a7
myuon <ioi.joi.koi.loi@gmail.com>
2018-09-15 11:04:18 +0900

    feat: creating index file

--- Makefile
+++ Makefile
@@ -1,6 +1,11 @@
-run: clean
+run:
 	stack build --fast && stack exec gen-docs ghc/compiler
+
+serve:
+	docsify serve ./docs
 
 clean:
 	rm -r docs
+	mkdir docs
+	docsify init ./docs
 
--- src/Main.hs
+++ src/Main.hs
@@ -18,16 +18,26 @@ getBlockComments text = do
 isPragma :: T.Text -> Bool
 isPragma t = not (T.null t) && T.head t == '#'
 
-foldOnDir :: FilePath -> (FilePath -> T.Text -> IO ()) -> IO ()
-foldOnDir base process = do
+createIndex :: FilePath -> [FilePath] -> IO ()
+createIndex base paths = do
+  let path = Path.joinPath [base, "README.md"]
+  Dir.createDirectoryIfMissing True base
+  TIO.writeFile path $ T.unlines $
+    ("# " `T.append` T.pack base) : "" : map (("- " `T.append`) . T.pack) paths
+
+foldOnDir :: FilePath -> (FilePath -> T.Text -> IO ()) -> (FilePath -> [FilePath] -> IO ()) -> IO ()
+foldOnDir base onFile onDir = do
   paths <- Dir.listDirectory base
+  onDir base paths
 
   forM_ paths $ \filename -> do
     let path = Path.joinPath [base, filename]
 
     isFile <- Dir.doesFileExist path
-    if isFile then process path =<< TIO.readFile path
-    else foldOnDir path process
+    if isFile then do
+      when (Path.takeExtension path == ".hs") $ do
+        onFile path =<< TIO.readFile path
+    else foldOnDir path onFile onDir
 
 main = do
   (path:_) <- getArgs
@@ -35,9 +45,12 @@ main = do
 
   let outDir = "docs"
 
-  foldOnDir path $ \filename content -> do
-    let path = Path.replaceExtensions (Path.joinPath [outDir, filename]) ".md"
-    Dir.createDirectoryIfMissing True (Path.dropFileName path)
+  foldOnDir path
+    (\filename content -> do
+        let path = Path.replaceExtensions (Path.joinPath [outDir, filename]) ".md"
+        Dir.createDirectoryIfMissing True (Path.dropFileName path)
 
-    TIO.writeFile path $ T.unlines $ filter (\c -> not (T.null c) && not (isPragma c)) $ getBlockComments content
+        TIO.writeFile path $ T.unlines $ filter (\c -> not (T.null c) && not (isPragma c)) $ getBlockComments content)
+    (\base paths -> do
+        createIndex (Path.joinPath [outDir, base]) paths)
 
